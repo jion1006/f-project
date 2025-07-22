ea6409428e5730c80154ecdbb7ed59fa251e7501
jion1006 <mfe1006@gmail.com>
2025-07-22 21:29:29 +0900

    ShopSlotUI추가
    기본적인 상품의 이미지, 이름, 가격등 추가 후 버튼을 통한 구매를 구현할 예정
    프리팹화 후 스크롤을 사용해 구현 예정

--- Assets/Scripts/Shop/ShopSlotUI.cs
+++ Assets/Scripts/Shop/ShopSlotUI.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class ShopSlotUI : MonoBehaviour
 {
+    public TMP_Text itemName;
+    public TMP_Text itemPrice;
+
+    public ShopItem shopItem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void BuyShopItem()
+    {
+        if (shopItem.price <= PlayerController.Instance.playerStat.coin)
+        {
+
+        }
+    }
+
+    public void SetItem(ShopItem _shopItem)
+    {
+        shopItem = _shopItem;
+        itemName.text = _shopItem.item.itemName;
+        itemPrice.text = _shopItem.price.ToString();
     }
 }
